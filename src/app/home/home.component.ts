b962239173ca1da8a4ade3969efb0a0325f3e31e
Ana Helena <anahelenarp@hotmail.com>
2019-03-05 20:57:43 -0300

    add import { Http } from "@angular/http"; (will be changed later)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
+import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Array<Oferta>;
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-  }
+    // this.ofertas = this.ofertasService.getOfertas();
 
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Array<Oferta>) => {
+          this.ofertas = ofertas;
+      });
+  }
 }
