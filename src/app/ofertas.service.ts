43bc68e230cf7ab6739b04c1d7ce617e9113c1b7
Ana Helena <anahelenarp@hotmail.com>
2019-04-21 17:14:26 -0300

    Add pesquisa de ofertas

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,5 +1,5 @@
 import { Oferta } from './shared/oferta.model';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, retry } from 'rxjs/operators';
@@ -14,31 +14,31 @@ export class OfertasService {
     public getOfertas(): Promise<Array<Oferta>> {
         return this.http.get(`${this.url}/ofertas`)
                         .toPromise()
-                        .then((resposta: any) => resposta.json());
+                        .then((resposta: Response) => resposta.json());
     }
 
     public getOfertasComDestaque(): Promise<Array<Oferta>> {
         return this.http.get(`${this.url}/ofertas?destaque=true`)
                         .toPromise()
-                        .then((resposta: any) => resposta.json());
+                        .then((resposta: Response) => resposta.json());
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
         return this.http.get(`${this.url}/ofertas?categoria=${categoria}`)
                         .toPromise()
-                        .then((resposta: any) => resposta.json());
+                        .then((resposta: Response) => resposta.json());
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
         return this.http.get(`${this.url}/ofertas?id[]=${id}`)
                         .toPromise()
-                        .then((resposta: any) => resposta.json().shift());
+                        .then((resposta: Response) => resposta.json().shift());
     }
 
     public getComoUsarOfertaPorId(id: number): Promise<string> {
         return this.http.get(`${this.url}/como-usar?id[]=${id}`)
         .toPromise()
-        .then((resposta: any) => {
+        .then((resposta: Response) => {
             const comoUsar = resposta.json().shift();
             return comoUsar.descricao;
         });
@@ -47,7 +47,7 @@ export class OfertasService {
     public getOndeFicaOfertaPorId(id: number): Promise<string> {
         return this.http.get(`${this.url}/onde-fica?id[]=${id}`)
         .toPromise()
-        .then((resposta: any) => {
+        .then((resposta: Response) => {
             const ondeFica = resposta.json().shift();
             return ondeFica.descricao;
         });
@@ -56,7 +56,7 @@ export class OfertasService {
     public pesquisarOfertas(valor: string): Observable<Oferta[]> {
         return this.http.get(`${this.url}/ofertas?descricao_oferta_like=${valor}`)
         .pipe(
-                map((resposta: any) => resposta.json()),
+                map((resposta: Response) => resposta.json()),
                 retry(10)
              );
     }
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,6 +1,7 @@
 import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
 import { Oferta } from '../shared/oferta.model';
 
 @Component({
@@ -14,17 +15,41 @@ export class TopoComponent implements OnInit {
   constructor(private service: OfertasService) { }
 
   ofertas: Observable<Oferta[]>;
+  ofertasPesquisa: Oferta[];
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+    .pipe(
+        debounceTime(1000), // 1 segundo
+        distinctUntilChanged(),
+        switchMap(valorPesquisa => {
+          if (valorPesquisa.trim() !== '') {
+            return this.service.pesquisarOfertas(valorPesquisa);
+          }
+
+          return of<Oferta[]>([]);
+        }),
+        catchError((err: any) => {
+          console.log(err);
+          return of<Oferta[]>([]);
+        })
+    );
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      this.ofertasPesquisa = ofertas;
+    });
   }
 
   public pesquisar(valorPesquisaOferta: string): void {
-    this.ofertas = this.service.pesquisarOfertas(valorPesquisaOferta);
+    /*this.ofertas = this.service.pesquisarOfertas(valorPesquisaOferta);
     this.ofertas.subscribe(
       (ofertas: Oferta[]) => { console.log(ofertas); },
       ((erro: any) => {  }),
       (() => { console.log('Fluxo de eventos completo'); })
-    );
+    );*/
+
+    this.subjectPesquisa.next(valorPesquisaOferta);
   }
 
 }
